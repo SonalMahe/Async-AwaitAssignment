72a0da4bff940d1c592dc5aef9a3c25683161d7a
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-02-06 13:04:50 +0100

    refactoring with async functions
    - done with 2 parts of assignment

--- asyncawait.ts
+++ asyncawait.ts
@@ -1,4 +1,4 @@
-//Part 1: Refactoring Your Own Promise with Async/Await  
+//Part 1: Refactoring Your Own Promise with Async/Await  -
 //Study Energy Level//-
 
 
@@ -7,41 +7,71 @@ const checkStudyEnergy = () => {
         let energyLevel = Math.random();
 
         if (energyLevel > 0.5) {
-            resolve("Energy level is high:) ! Ready to study.");
+            resolve("Energy level is high! Ready to study.");
         } else {
-            reject("Energy level is low :( .Take a break first.")
+            reject("Energy level is low. Take a break first.")
         }
     });
 }
-checkStudyEnergy()
-    .then((message) => {
-            console.log(message);
-        })
 
-   .catch((error) => {
-    console.log("Error:" + error);
-});
+const output = async () => {
+    try {
+        const message = await checkStudyEnergy();
+        console.log(message);
+    } catch (error) {
+        console.log(error);
+    }
+};
+output();
+
+// checkStudyEnergy()
+//     .then((message) => {
+//             console.log(message);
+//         })
+
+//    .catch((error) => {
+//     console.log("Error:" + error);
+// });
 
 
 
 // part 2.Fetching Data from an API- (using country details API)-
 
-const fetchCountryData = (countryname) => {
-const promise = fetch(`https://restcountries.com/v3.1/name/${countryname}`) 
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(" 🌍 Country :-" , data[0].name.official)
-        console.log(" 🏛️ Capital :-", data[0].capital[0])
-        console.log(" 🌐 Region :-", data[0].region)
-    })
-    .catch((error) => {
-        console.log('error fetching data:', error);
-    })
+const fetchCountryData = async () => {
+    try {
+        const response = await fetch(`https://restcountries.com/v3.1/name/${"Hungary"}`);
+        if (!response.ok) {
+            throw new Error("Response was not ok !")
+        }
+
+        const data = await response.json();
+        console.log("Country:-", data[0].name.official);
+        console.log("Capital:- ", data[0].capital);
+        console.log("Region:-", data[0].region)
+
+    } catch (error) {
+        console.log(error);
+
+    }
 };
 
-fetchCountryData("sweden");
-fetchCountryData("italy");
-fetchCountryData("france");
+fetchCountryData();
+
+// const result = await (`https://restcountries.com/v3.1/name/${countryname}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//         console.log(" 🌍 Country :-" , data[0].name.official)
+//         console.log(" 🏛️ Capital :-", data[0].capital[0])
+//         console.log(" 🌐 Region :-", data[0].region)
+//     })
+//     .catch((error) => {
+//         console.log('error fetching data:', error);
+//     })
+// };
+
+// fetchCountryData("sweden");
+// fetchCountryData("italy");
+// fetchCountryData("france");
 
 
 
